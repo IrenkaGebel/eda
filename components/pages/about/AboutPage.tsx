63532d055205152f07f50c641d21bb47eaf6fd02
irenka <nikoniuknatalia777@gmail.com>
2024-05-01 12:41:46 +0200

    updates the desing of about page after meeting 01/05

--- components/pages/about/AboutPage.tsx
+++ components/pages/about/AboutPage.tsx
@@ -3,6 +3,7 @@ import { PortableTextBlock } from 'sanity'
 
 import { CustomPortableText } from '@/components/shared/CustomPortableText'
 import type { AboutPayload } from '@/types'
+import Link from 'next/link'
 
 export interface AboutPageProps {
   data: AboutPayload | null
@@ -13,14 +14,14 @@ export function About({ data }: AboutPageProps) {
 
   return (
     <>
-      <div className=" flex flex-col snap-y snap-mandatory overflow-y-scroll h-dvh lg:gap-64">
+      <div className=" flex flex-col gap-36">
         <article
           id="Eda"
-          className="pl-4 pr-4 mb-16 lg:mb-0 lg:flex lg:flex-row  lg:gap-x-14 lg:justify-center h-dvh lg:pt-32 snap-always snap-start "
+          className="pl-4 pr-4 mb-16 lg:mb-0 lg:flex lg:flex-row  lg:gap-x-14 lg:justify-center lg:pt-32  "
         >
           {edaGroup && (
             <>
-              <section className="lg:w-1/3 ">
+              <section className="md:w-2/3 lg:w-1/3 ">
                 <div className="mt-8 mb-8 text-volcano-dust flex gap-8 items-baseline ">
                   <h1 className="text-6xl">{edaGroup.headingEda}</h1>
                   <h2 className="">{edaGroup.headingCaption}</h2>
@@ -30,7 +31,7 @@ export function About({ data }: AboutPageProps) {
                 />
               </section>
 
-              <section className="lg:pt-4">
+              <section className="lg:pt-4 lg:w-1/3">
                 <h1 className="mt-8 mb-8 text-volcano-dust text-5xl">
                   {edaGroup.headingContact}
                 </h1>
@@ -50,14 +51,19 @@ export function About({ data }: AboutPageProps) {
             </>
           )}
         </article>
+        {/* <div className="">
+          <Link href="#Iszszi" className="p-4 float-right">
+            isz szi studio ↓
+          </Link>
+        </div> */}
 
         <article
           id="Iszszi"
-          className="pl-4 lg:pl-12 pr-4 md:mb-32 lg:flex lg:flex-row lg:gap-x-14 lg:justify-center lg:h-dvh lg:mb-64 snap-always snap-start"
+          className="pl-4 pr-4 lg:flex lg:flex-row lg:gap-x-14  lg:justify-center lg:pb-64 "
         >
           {iszsziGroup && (
             <>
-              <section className="lg:w-1/3 lg:ml-8">
+              <section className="md:w-2/3 lg:w-1/3 ">
                 <h1 className="mt-8 mb-8 text-6xl text-volcano-dust  ">
                   {iszsziGroup.headingIszszi}
                 </h1>
